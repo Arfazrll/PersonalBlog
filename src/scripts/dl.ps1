33888c246e95b8997ede7d80281ba21dfc8b08ab
Arfazrll <azril4974@gmail.com>
2026-02-28 13:52:52 +0700

    feat: update home and lanyard

--- src/scripts/dl.ps1
+++
@@ -1,3 +0,0 @@
-New-Item -ItemType Directory -Force -Path "c:\app\PersonalBlog\public\lanyard"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidHDev/react-bits/main/src/assets/lanyard/card.glb" -OutFile "c:\app\PersonalBlog\public\lanyard\card.glb"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/DavidHDev/react-bits/main/src/assets/lanyard/lanyard.png" -OutFile "c:\app\PersonalBlog\public\lanyard\lanyard.png"
